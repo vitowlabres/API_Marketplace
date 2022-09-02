9367dfe0a58047d07b6552611d67eb3e76c5f6c9
AnaHermes <ana.rhermes@gmail.com>
2022-09-02 20:32:20 -0300

    ajuste documentacao inclusao filtro

---
+++ bdprojeto.js
@@ -0,0 +1,25 @@
+const itens = [
+    { id: '1cpb3', tipo: 'Camiseta manga curta', tamanho: 'P', cor: 'Branca', valor: 5990, estoque: 3},
+    { id: '1cgv4', tipo: 'Camiseta manga curta', tamanho: 'GG', cor: 'Verde', valor: 5990, estoque: 4},
+    { id: '1cma4', tipo: 'Camiseta manga longa', tamanho: 'M', cor: 'Azul', valor: 6990, estoque: 4},
+    { id: '1cpa1', tipo: 'Camiseta manga longa', tamanho: 'P', cor: 'Amarela', valor: 6990, estoque: 1},
+    { id: '1rgp3', tipo: 'Regata', tamanho: 'G', cor: 'Preta', valor: 4990, estoque: 3},
+    { id: '1rpv5', tipo: 'Regata', tamanho: 'P', cor: 'Vermelha', valor: 4990, estoque: 5},
+    { id: '1cpp1', tipo: 'Casaco moletom', tamanho: 'P', cor: 'Preta', valor: 12900, estoque: 1},
+    { id: '1cgc6', tipo: 'Casaco moletom', tamanho: 'G', cor: 'Cinza', valor: 13900, estoque: 6},
+    { id: '1vmp2', tipo: 'Vestido', tamanho: 'M', cor: 'Preto', valor: 11900, estoque: 2},
+    { id: '1vpr3', tipo: 'Vestido', tamanho: 'P', cor: 'Rosa', valor: 15900, estoque: 3},
+    { id: '1bgc1', tipo: 'Bermuda', tamanho: 'GG', cor: 'Cinza', valor: 8400, estoque: 1},
+    { id: '1bgm2', tipo: 'Bermuda', tamanho: 'G', cor: 'Marrom', valor: 8400, estoque: 2},
+    { id: '1smv2', tipo: 'Short jeans', tamanho: 'M', cor: 'Vermelho', valor: 7200, estoque: 2},
+    { id: '1sgb3', tipo: 'Short jeans', tamanho: 'G', cor: 'Branco', valor: 7600, estoque: 3},
+    { id: '1jpp2', tipo: 'Jaqueta de couro', tamanho: 'P', cor: 'Preta', valor: 22200, estoque: 2},
+    { id: '1jmp4', tipo: 'Jaqueta de couro', tamanho: 'M', cor: 'Preta', valor: 22200, estoque: 4},
+    { id: '1jgm3', tipo: 'Jaqueta de couro', tamanho: 'G', cor: 'Marrom', valor: 22900, estoque: 3},
+    { id: '1cma3', tipo: 'Calça jeans', tamanho: 'M', cor: 'Azul', valor: 14900, estoque: 3},
+    { id: '1cgp2', tipo: 'Calça jeans', tamanho: 'G', cor: 'Preta', valor: 14900, estoque: 2},
+    { id: '1cgc2', tipo: 'Calça moletom', tamanho: 'GG', cor: 'Cinza', valor: 9900, estoque: 2
+    }
+]
+
+export default itens
